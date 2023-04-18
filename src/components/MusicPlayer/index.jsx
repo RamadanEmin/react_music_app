c9e1f4f09083dfcf889798fa717bd333326f7cba
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-04-18 22:40:04 +0300

    Added Music player "Controls" component

--- src/components/MusicPlayer/index.jsx
+++ src/components/MusicPlayer/index.jsx
@@ -1,20 +1,67 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
-import { playPause } from '../../redux/features/playerSlice';
+import { nextSong, prevSong, playPause } from '../../redux/features/playerSlice';
+import Controls from './Controls';
 import Track from './Track';
 
 const MusicPlayer = () => {
     const { activeSong, currentSongs, currentIndex, isActive, isPlaying } = useSelector((state) => state.player);
+    const [repeat, setRepeat] = useState(false);
+    const [shuffle, setShuffle] = useState(false);
     const dispatch = useDispatch();
 
     useEffect(() => {
         if (currentSongs.length) dispatch(playPause(true));
     }, [currentIndex]);
 
+    const handlePlayPause = () => {
+        if (!isActive) return;
+
+        if (isPlaying) {
+            dispatch(playPause(false));
+        } else {
+            dispatch(playPause(true));
+        }
+    };
+
+    const handleNextSong = () => {
+        dispatch(playPause(false));
+
+        if (!shuffle) {
+            dispatch(nextSong((currentIndex + 1) % currentSongs.length));
+        } else {
+            dispatch(nextSong(Math.floor(Math.random() * currentSongs.length)));
+        }
+    };
+
+    const handlePrevSong = () => {
+        if (currentIndex === 0) {
+            dispatch(prevSong(currentSongs.length - 1));
+        } else if (shuffle) {
+            dispatch(prevSong(Math.floor(Math.random() * currentSongs.length)));
+        } else {
+            dispatch(prevSong(currentIndex - 1));
+        }
+    };
+
     return (
         <div className="relative sm:px-12 px-8 w-full flex items-center justify-between">
             <Track isPlaying={isPlaying} isActive={isActive} activeSong={activeSong} />
+            <div className="flex-1 flex flex-col items-center justify-center">
+                <Controls
+                    isPlaying={isPlaying}
+                    isActive={isActive}
+                    repeat={repeat}
+                    setRepeat={setRepeat}
+                    shuffle={shuffle}
+                    setShuffle={setShuffle}
+                    currentSongs={currentSongs}
+                    handlePlayPause={handlePlayPause}
+                    handlePrevSong={handlePrevSong}
+                    handleNextSong={handleNextSong}
+                />
+            </div>
         </div>
     );
 };
