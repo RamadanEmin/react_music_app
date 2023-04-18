d4f70ab0dd6ecd91fb02c550c7fb3d4643ac6fcc
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-04-18 18:55:37 +0300

    Added Discover page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,23 @@
+import { useSelector } from 'react-redux';
+import { Route, Routes } from 'react-router-dom';
+
+import { Discover } from './pages';
+
 const App = () => {
+    const { activeSong } = useSelector((state) => state.player);
+
     return (
-     
+        <div className="relative flex">
+            <div className="flex-1 flex flex-col bg-gradient-to-br from-black to-[#121286]">
+                <div className="px-6 h-[calc(100vh-72px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse">
+                    <div className="flex-1 h-fit pb-40">
+                        <Routes>
+                            <Route path="/" element={<Discover />} />
+                        </Routes>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 };
 
---
+++ src/pages/Discover.jsx
@@ -0,0 +1,35 @@
+import { useDispatch, useSelector } from 'react-redux';
+
+import { genres } from '../assets/constants';
+
+import { selectGenreListId } from '../redux/features/playerSlice';
+
+const Discover = () => {
+    const dispatch = useDispatch();
+    const { activeSong, isPlaying, genreListId } = useSelector((state) => state.player);
+
+    const genreTitle = genres.find(({ value }) => value === genreListId)?.title;
+
+    return (
+        <div className="flex flex-col">
+            <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
+                <h2 className="font-bold text-3xl text-white text-left">
+                    Discover {genreTitle}
+                </h2>
+                <select
+                    onChange={(e) => dispatch(selectGenreListId(e.target.value))}
+                    value={genreListId || 'pop'}
+                    className="bg-black text-gray-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5"
+                >
+                    {genres.map((genre) => (
+                        <option key={genre.value} value={genre.value}>
+                            {genre.title}
+                        </option>
+                    ))}
+                </select>
+            </div>
+        </div>
+    );
+};
+
+export default Discover;
