dc11e2a44246449c5430a2391a7bb6f6b08da325
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-04-19 13:31:20 +0300

    Fetch 'top charts' data from Shazam Core API

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -1,15 +1,22 @@
 import { useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { FreeMode } from 'swiper';
+
+import { useGetTopChartsQuery } from '../redux/services/shazamCore';
 
 import "swiper/css";
 import "swiper/css/free-mode";
 
 const TopPlay = () => {
+    const { data } = useGetTopChartsQuery();
     const divRef = useRef(null);
 
     useEffect(() => {
         divRef.current.scrollIntoView({ behavior: 'smooth' });
     });
+
+    const topPlays = data?.slice(0, 5);
 
     return (
         <div
@@ -30,6 +37,31 @@ const TopPlay = () => {
                             <p className="text-gray-300 text-base cursor-pointer">See more</p>
                         </Link>
                     </div>
+                    <Swiper
+                        slidesPerView="auto"
+                        spaceBetween={15}
+                        freeMode
+                        centeredSlides
+                        centeredSlidesBounds
+                        modules={[FreeMode]}
+                        className="mt-4 "
+                    >
+                        {topPlays?.map((song) => (
+                            <SwiperSlide
+                                key={song.key}
+                                style={{ width: '25%', height: 'auto' }}
+                                className="shadow-lg rounded-full animate-slideright"
+                            >
+                                <Link to={song.artists ? `/artists/${song?.artists[0]?.adamid}` : ''}>
+                                    <img
+                                        src={song?.images?.background}
+                                        alt="name"
+                                        className="rounded-full w-full object-cover"
+                                    />
+                                </Link>
+                            </SwiperSlide>
+                        ))}
+                    </Swiper>
                 </div>
             </div>
         </div>
--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -1,20 +1,22 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const shazamCoreApi = createApi({
-  reducerPath: 'shazamCoreApi',
-  baseQuery: fetchBaseQuery({
-    baseUrl: 'https://shazam-core.p.rapidapi.com/',
-    prepareHeaders: (headers) => {
-      headers.set('X-RapidAPI-Key', process.env.VITE_SHAZAM_CORE_RAPID_API_KEY);
+    reducerPath: 'shazamCoreApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'https://shazam-core.p.rapidapi.com/',
+        prepareHeaders: (headers) => {
+            headers.set('X-RapidAPI-Key', process.env.VITE_SHAZAM_CORE_RAPID_API_KEY);
 
-      return headers;
-    },
-  }),
-  endpoints: (builder) => ({
-    getSongsByGenre: builder.query({ query: (genre) => `v1/charts/genre-world?genre_code=${genre}` })
-  }),
+            return headers;
+        },
+    }),
+    endpoints: (builder) => ({
+        getTopCharts: builder.query({ query: () => 'v1/charts/world' }),
+        getSongsByGenre: builder.query({ query: (genre) => `v1/charts/genre-world?genre_code=${genre}` }),
+    }),
 });
 
 export const {
-  useGetSongsByGenreQuery
+    useGetTopChartsQuery,
+    useGetSongsByGenreQuery,
 } = shazamCoreApi;
