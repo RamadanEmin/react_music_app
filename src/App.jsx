307d183d4b205485f673f1bfaded5097748ecd80
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-04-19 10:18:58 +0300

    Added "Side bar" component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useSelector } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
 
-import { MusicPlayer } from './components';
+import { Sidebar, MusicPlayer } from './components';
 import { Discover } from './pages';
 
 const App = () => {
@@ -9,6 +9,7 @@ const App = () => {
 
     return (
         <div className="relative flex">
+            <Sidebar />
             <div className="flex-1 flex flex-col bg-gradient-to-br from-black to-[#121286]">
                 <div className="px-6 h-[calc(100vh-72px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse">
                     <div className="flex-1 h-fit pb-40">
