11146cbc93e6b80d949b0750621cfa8c7a0aa4d9
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-04-20 07:58:14 +0300

    Added "Related songs" component

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -1,6 +1,6 @@
 import { useParams } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
-import { DetailsHeader, Error, Loader } from '../components';
+import { DetailsHeader, Error, Loader, RelatedSongs } from '../components';
 
 import { setActiveSong, playPause } from '../redux/features/playerSlice';
 import {
