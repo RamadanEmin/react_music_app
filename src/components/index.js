5135729941bd3ce5594bed21e9edae49a5ee2ccd
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-04-20 08:29:58 +0300

    Added "Song bar" component

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,22 @@
+import Sidebar from './Sidebar';
 import SongCard from './SongCard';
+import TopPlay from './TopPlay';
+import ArtistCard from './ArtistCard';
+import DetailsHeader from './DetailsHeader';
+import SongBar from './SongBar';
+import RelatedSongs from './RelatedSongs';
 import MusicPlayer from './MusicPlayer';
 import Loader from './Loader';
 import Error from './Error';
 
 export {
+    TopPlay,
+    Sidebar,
     SongCard,
+    ArtistCard,
+    DetailsHeader,
+    SongBar,
+    RelatedSongs,
     MusicPlayer,
     Loader,
     Error,
